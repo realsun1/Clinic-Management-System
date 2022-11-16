931ee03f14f168c5a410d1e42147e8da77dbc24f
sundin <sundin.nguyen@gmail.com>
2022-11-15 22:04:13 -0500

    connect register/login to db

--- src/UserDatabase/DataManager.java
+++ src/UserDatabase/DataManager.java
@@ -10,7 +10,7 @@ public class DataManager {
     static private String driver = "com.mysql.cj.jdbc.Driver";
     static private String url = "jdbc:mysql://localhost:3306/clinicsystem?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     static private String user = "root";
-    static private String pwd = "123456";
+    static private String pwd = "";
 
     // creator for manager, connect to the database
     public DataManager() throws ClassNotFoundException, SQLException {
@@ -48,15 +48,32 @@ public class DataManager {
         return stat.execute(sql);
     }
 
-    // public static void main(String[] args) throws ClassNotFoundException,
-    // SQLException {
-    // manager mg = new manager();
-    // /*String add = "insert into doctor(dname, dno, location)"
-    // + "values(\"erc\",\"strb1\",\"dstn\");";
-    // mg.execute(add);*/
-    // String query = "select * from doctor";
-    // List<String> display = mg.query(query);
-    // System.out.println(display.get(0));
-    // System.out.println(display.get(1));
-    // }
+    // Establish a connection to the database.
+    public static Connection getConnection() {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        try {
+            con = DriverManager.getConnection(url, user, pwd);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException,
+    SQLException {
+    //     DataManager mg = new DataManager();
+    //     /*String add = "insert into doctor(dname, dno, location)"
+    //     + "values(\"erc\",\"strb1\",\"dstn\");";
+    //     mg.execute(add);*/
+    //     String query = "select * from login_info";
+    //     List<String> display = mg.query(query);
+    //     System.out.println(display.get(0));
+    //    // System.out.println(display.get(1));
+    }
 }
