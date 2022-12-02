b7df2c346d24aa739919389d9d5d838f96ef7c03
BruceLee0212 <826635507@qq.com>
2022-12-02 18:21:33 -0500

    update deleteDoctorByID in Admin.java

--- src/User/Admin.java
+++ src/User/Admin.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import UserDatabase.DataManager;
 import Validator.Validator;
@@ -40,7 +41,7 @@ public class Admin {
             System.out.println("There are no doctors to display.");
             return;
         }
-        
+
         System.out.printf("%-15s", "ID");
         System.out.printf("%-20s", "Name");
         System.out.printf("%-10s", "Location");
@@ -92,10 +93,39 @@ public class Admin {
         }
     }
 
+    // delete doctor by doctor's id
+    public void deleteDoctorByID() {
+        System.out.println("Please Enter the Doctor ID You Want To Delete:");
+        String dno = in.nextLine();
+        Pattern pattern = Pattern.compile("[0-9]+");
+        while (!pattern.matcher(dno).matches()) {
+            System.out.println("Doctor ID Must Be Digits Only. Please Re-enter:");
+            dno = in.nextLine();
+        }
+        String sql1 = "select * from user where doctor_number=" + Integer.parseInt(dno) + ";";
+        List<String> ret = new ArrayList<>();
+        try {
+            ret = manager.query(sql1);
+        } catch (SQLException e) {
+            System.err.println("Error: (" + e.getMessage() + ").");
+        }
+        if (ret.size() == 0) {
+            System.out.println("Doctor Not Found.");
+            return;
+        } else {
+            String sql2 = "delete from user where doctor_number=" + Integer.parseInt(dno) + ";";
+            try {
+                manager.execute(sql2);
+            } catch (SQLException e) {
+                System.err.println("Error: (" + e.getMessage() + ").");
+            }
+            System.out.println("Delete Successfully.");
+        }
+    }
+
     // public static void main(String[] args) throws ClassNotFoundException,
     // SQLException {
     // Admin admin = new Admin(new Scanner(System.in), new DataManager());
-    // admin.printDoctors();
-    // admin.printReceptionists();
+    // admin.deleteDoctorByID();
     // }
 }
